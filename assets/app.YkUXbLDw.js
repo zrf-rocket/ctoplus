0c140000e9609bb10f69413cbb0e8616984fe319
SteveRocket <rocket_2014@126.com>
2024-08-31 01:59:48 +0800

    新增内容

---
+++ assets/app.YkUXbLDw.js
@@ -0,0 +1,7 @@
+import{j as s,aa as p,ab as u,ac as l,ad as c,ae as f,af as d,ag as m,ah as h,ai as A,aj as g,ak as v,d as P,u as w,l as y,z as C,al as R,am as _,an as b,ao as E}from"./chunks/framework.NMZE9xmb.js";import{R as j}from"./chunks/theme.J-kG361f.js";function i(e){if(e.extends){const a=i(e.extends);return{...a,...e,async enhanceApp(t){a.enhanceApp&&await a.enhanceApp(t),e.enhanceApp&&await e.enhanceApp(t)}}}return e}const o=i(j),D=P({name:"VitePressApp",setup(){const{site:e,lang:a,dir:t}=w();return y(()=>{C(()=>{document.documentElement.lang=a.value,document.documentElement.dir=t.value})}),e.value.router.prefetchLinks&&R(),_(),b(),o.setup&&o.setup(),()=>E(o.Layout)}});async function T(){const e=O(),a=L();a.provide(u,e);const t=l(e.route);return a.provide(c,t),a.component("Content",f),a.component("ClientOnly",d),Object.defineProperties(a.config.globalProperties,{$frontmatter:{get(){return t.frontmatter.value}},$params:{get(){return t.page.value.params}}}),o.enhanceApp&&await o.enhanceApp({app:a,router:e,siteData:m}),{app:a,router:e,data:t}}function L(){return h(D)}function O(){let e=s,a;return A(t=>{let n=g(t),r=null;return n&&(e&&(a=n),(e||a===n)&&(n=n.replace(/\.js$/,".lean.js")),r=v(()=>import(n),__vite__mapDeps([]))),s&&(e=!1),r},o.NotFound)}s&&T().then(({app:e,router:a,data:t})=>{a.go().then(()=>{p(a.route,t.site),e.mount("#app")})});export{T as createApp};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
---
+++ assets/chunks/wechat.C3sc3AY6.js
@@ -0,0 +1,1 @@
+const s="/assets/wechat.uai309nT.jpg";export{s as _};
